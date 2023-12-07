762ddc1ace037ea850cba29d8e30f5a10de5aa48
xinweizheng1122 <xinweizh@bu.edu>
2023-12-07 12:47:24 -0500

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,44 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+
+let audio_controls = document.querySelectorAll('.audio-controls')
+audio_controls.forEach((item, index) => {
+	item.addEventListener('play', () => {
+		audio_controls.forEach((el, currentIndex) => {
+			if (index !== currentIndex) {
+				el.pause()
+				el.currentTime = 0
+			}
+		})
+		item.play()
+	})
+})
+
+let icon = document.querySelector('.icon')
+let bg = document.querySelector('.liquid img')
+let title_h1 = document.querySelector('.title-h1')
+let title_a = document.querySelector('.title-a')
+let introduce = document.querySelector('.introduce')
+let liquid = document.querySelector('.liquid')
+
+let opener = false
+icon.onclick = () => {
+	if (opener == false) {
+		bg.classList.add('bg-change')
+		title_h1.classList.add('h1-change')
+		title_a.classList.add('a-change')
+		introduce.classList.add('introduce-change')
+		opener = true
+		return
+	}
+	bg.classList.remove('bg-change')
+	title_h1.classList.remove('h1-change')
+	title_a.classList.remove('a-change')
+	introduce.classList.remove('introduce-change')
+	opener = false
+}
+
+
+
