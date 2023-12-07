b634f93ab7682e3d10a4a25e92dc29bb0974d08a
xinweizheng1122 <xinweizh@bu.edu>
2023-12-07 14:01:52 -0500

    final website

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -44,9 +44,6 @@ icon.onclick = () => {
 	opener = false
 }
 
-document.querySelectorAll("section .item a").addEventListener("mousemove", function (e) {
-	document.documentElement.style.setProperty('--cursor-x', e.clientX + 'px');
-	document.documentElement.style.setProperty('--cursor-y', e.clientY + 'px');
-});
+
 
 
